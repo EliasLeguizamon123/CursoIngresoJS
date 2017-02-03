e22eb7611b1a749da0693fe7bd9b84e0be0b26e5
EliasLeguizamon123 <elias-1997@live.com.ar>
2017-02-03 11:41:52 -0300

    Update jsEntradaSalida-6.js

--- jsEntradaSalida-6.js
+++ jsEntradaSalida-6.js
@@ -6,9 +6,7 @@ function sumar()
 var numeroUno;
 var numeroDos;
 var resultado;
-numeroUno=prompt("Ingrese el numero uno");
-numeroDos=prompt("Ingrese el numero dos");
-numeroUno=document.getElementById('numeroUno').value;
+numeroUno=numeroUno=document.getElementById('numeroUno').value;
 numeroDos=document.getElementById('numeroDos').value;
 numeroUno=parseInt(numeroUno);
 numeroDos=parseInt(numeroDos);
