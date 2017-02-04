43d7539f83c0b07c88d87ffc00cb5909063ae3b6
EliasLeguizamon123 <elias-1997@live.com.ar>
2017-02-03 21:20:20 -0300

    ultima parte de la carpeta "entrada y salida"

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,22 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
+	/*var resultado;
+	resultado=2%10;
+	alert(resultado); //no sirve para los numeros primos.
+	resultado=10%2;
+	alert(resultado);*/
+
+	var resultado;
+	var numeroUno;
+	var numeroDos;
+
+	numeroUno=document.getElementById('numeroDividendo').value;
+	numeroDos=document.getElementById('numeroDivisor').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+	resultado=numeroUno%numeroDos;
+	alert("el resultado es: "+resultado);
 	
+
 }
