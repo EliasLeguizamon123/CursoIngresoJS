5ab64900e8cbbef59a4dc8ec551845135518c4ec
EliasLeguizamon123 <elias-1997@live.com.ar>
2017-02-09 22:08:54 -0300

    ejercicios hechos

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,21 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
+	var estadoCivil;
+	var Soltero;
 
-	
+	edad=document.getElementById('edad').value;
+    /*soltero=document.getElementById('Soltero').value;
+    /*casado=document.getElementById('Casado').value;
+    divorciado=document.getElementById('Divorciado').value;*/
+    estadiCivil=document.getElementById('estadoCivil').value;
+    if (edad<18 && estadoCivil!="soltero")
+    	{
+         alert("es muy pequeño para NO ser soltero")
+    	}
+    	
+
 
 
 }//FIN DE LA FUNCIÓN
