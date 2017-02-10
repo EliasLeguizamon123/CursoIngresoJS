a985b534e4a522209c617b5a8504ac5f1e8f01ed
EliasLeguizamon123 <elias-1997@live.com.ar>
2017-02-10 20:16:48 -0300

    ejercicio 3 correjido

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -6,24 +6,12 @@ var mesDelAño = document.getElementById('mes').value;
 //alert (mesDelAño);
 switch(mesDelAño)
 		{
-			case "Enero":
-			alert("Este mes tiene 30 dias o mas");
-			break;
+			
 			case "Febrero":
 			alert("Este mes no tiene mas de 30 dias");
 			break;
-			case "Marzo":
-			case "Abril":
-			case "Mayo":
-			case "Junio":
-			case "Julio":
-			case "Agosto":
-			case "Septiembre":
-			case "Octubre":
-			case "Noviembre":
-			case "Diciembre":
-			alert("Este mes tiene 30 dias o mas");
-			break;
+			default:                               //Default sirve para "Negar" lo que esta dentro del switch
+			alert("Este mes tiene 30 dias o mas"); // en otras palabras es un "Else" para el switch
 			
 
 		}
