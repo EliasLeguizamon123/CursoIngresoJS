173c349c85b9efe1fcdaed650a80cf84ec9f39a9
EliasLeguizamon123 <elias-1997@live.com.ar>
2017-03-08 13:40:51 -0300

    parcial

--- jsEntradaSalida-5.js
+++ jsEntradaSalida-5.js
@@ -1,14 +1,95 @@
 /*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
-function Mostar()
-{	
-	var nombre;
-	var edad;
-	nombre=prompt("Ingrese su nombre");
-	edad=prompt("Ingrese su edad");
-	document.getElementById('elNombre').value=nombre;
-	document.getElementById('laEdad').value=edad;
-	alert("Usted se llama "+nombre+" y tiene "+edad+" años);
+	function Mostar()
+	{	
+		/*  Ejercicio Modelo de Examen 1
+		var ladoCuadrado=document.getElementById('elNombre').value;
+		ladoCuadrado=parseInt(ladoCuadrado);
+		var perimetroCuadrado;
 
-}
+		perimetroCuadrado=(ladoCuadrado+ladoCuadrado+ladoCuadrado+ladoCuadrado);
+		alert("el perimetro de su cuadrado es: "+perimetroCuadrado);*/
+
+		/* Ejercicio Modelo de Examen 2
+		var resultado;
+		var importe;
+
+		importe=prompt("Ingrese un importe para que sea sumado el IVA(21%)");
+		importe=parseInt(importe);
+		resultado=(importe*21)/100;
+		alert("Su importe con la suma del IVA(21%) es: "+resultado);*/
+
+		/* Ejercicio Modelo de Examen 3
+		var largoTerreno;
+		var anchoTerreno;
+		var perimetro;
+		var perimetroAlambre;
+
+		alert("ingrese el largo y el ancho del terreno para sacar un perimetro");
+		largoTerreno=document.getElementById('elNombre').value;
+		largoTerreno=parseInt(largoTerreno);
+		anchoTerreno=document.getElementById('laEdad').value;
+		anchoTerreno=parseInt(anchoTerreno);
+		perimetro=anchoTerreno*largoTerreno;
+		perimetroAlambre=perimetro*3;
+		alert("se necesitan "+perimetroAlambre+" metros de alambre para colocarlos en el terreno");*/
+
+
+ 		/* Ejercicio Modelo de Examen 4
+		var numeroUno;
+		var numeroDos;
+		var resultado;
+
+		numeroUno=prompt("ingrese el primer numero");
+		numeroUno=parseInt(numeroUno);
+		numeroDos=prompt("ingrese el segundo numero");
+		numeroDos=parseInt(numeroDos);
+
+		if (numeroUno==numeroDos)
+			{	
+				resultado=numeroUno*numeroDos;
+				document.write(resultado);
+			}
+			else if(numeroUno>numeroDos)
+				{
+					resultado=numeroUno-numeroDos;
+					document.write(resultado);
+				}
+				else
+					{
+						resultado=numeroUno+numeroDos;
+						document.write(resultado);
+					} */
+
+		/*  Ejercicio Modelo de Examen 5
+		var diaDeLaSemana = prompt("ingrese que dia de la semana es");
+		switch (diaDeLaSemana)
+			{
+				case "sabado":
+				alert("es fin de semana");
+				break;
+
+				case "domingo":
+				alert("es fin de semana");
+				break;
+
+				default:
+				alert("a trabajar!!");
+				break;
+
+			}*/
+
+
+
+
+		
+
+
+
+
+
+
+
+		
+	}
 
